c55ae302969dc26d99a2559aebdc1e07072fd7d6
Yuss9 <yurtseven68210@hotmail.fr>
2023-11-15 08:48:01 +0100

    feat: fix problem

--- client.py
+++ client.py
@@ -1,24 +1,49 @@
 import socket
 import ssl
+from cryptography import x509
+from cryptography.hazmat.backends import default_backend
 
 # Paramètres du client
-HOST = 'www.yurtseven.fr'  # Adresse IP du serveur
+HOST = 'www.pirate.fr'  # Adresse IP du serveur
 PORT = 12345         # Port d'écoute du serveur
-CERTFILE = 'serveur_http.cert.pem'  # Chemin vers le certificat du client
-KEYFILE = 'serveur_http.pem'   # Chemin vers la clé privée du client
 
-# Création du socket client SSL
+# Création du socket client
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((HOST, PORT))
 
-# Configuration du contexte SSL
+# Création du contexte SSL
 context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
-context.load_cert_chain(certfile=CERTFILE, keyfile=KEYFILE)
 
-# Connexion au serveur
+# Ajout de la vérification du certificat en spécifiant le fichier de certificat de l'émetteur (Root CA)
+context.load_verify_locations(cafile='root_ca.cert.pem')
+
+# Wrapping du socket dans SSL en utilisant le contexte SSL
 secure_client_socket = context.wrap_socket(client_socket, server_hostname=HOST)
-secure_client_socket.connect((HOST, PORT))
 
-print(f"Connecté au serveur {HOST}:{PORT}")
+# Réception du certificat du serveur
+server_cert_bytes = secure_client_socket.recv(4096)
+server_cert = x509.load_pem_x509_certificate(
+    server_cert_bytes, default_backend())
+
+# Vérification du certificat
+
+# Ajoutez ici votre logique de vérification du certificat (nom de domaine, date, CA, etc.)
+print(f"Certificat du serveur décodé:\n{server_cert}")
+
+# Extraction du Common Name (CN) du certificat
+common_name = server_cert.subject.get_attributes_for_oid(
+    x509.NameOID.COMMON_NAME)[0].value
+
+# Comparaison avec la variable HOST
+if common_name == HOST:
+    print(f"Le Common Name (CN) du certificat correspond à {HOST}")
+else:
+    print(f"Le Common Name (CN) du certificat ne correspond pas à {HOST}")
+    # Annulation de la connexion
+    secure_client_socket.close()
+    sys.exit(
+        "La connexion est annulée en raison d'une non-correspondance du Common Name (CN).")
+
 
 while True:
     # Envoi de données au serveur
--- serveur.py
+++ serveur.py
@@ -4,40 +4,56 @@ import ssl
 # Paramètres du serveur
 HOST = '127.0.0.1'  # Adresse IP du serveur
 PORT = 12345         # Port d'écoute du serveur
-CERTFILE = 'serveur_http.cert.pem'  # Chemin vers le certificat du client
-KEYFILE = 'serveur_http.pem'   # Chemin vers la clé privée du client
+CERTFILE = 'serveur_http.cert.pem'  # Chemin vers le certificat du serveur
+KEYFILE = 'serveur_http.pem'   # Chemin vers la clé privée du serveur
 
-# Création du socket serveur SSL
+# Création du socket serveur
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((HOST, PORT))
 server_socket.listen()
 
-# Configuration du contexte SSL
-context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-context.load_cert_chain(certfile=CERTFILE, keyfile=KEYFILE)
-
 print(f"Le serveur écoute sur {HOST}:{PORT}")
 
-# Attente de la connexion d'un client
-client_socket, client_address = server_socket.accept()
-print(f"Connexion établie avec {client_address}")
+# Initialisation de secure_client_socket en dehors du bloc try
+secure_client_socket = None
 
-# Wrapping du socket dans le contexte SSL
-secure_client_socket = context.wrap_socket(client_socket, server_side=True)
+try:
+    # Attente de la connexion d'un client
+    client_socket, client_address = server_socket.accept()
+    print(f"Connexion établie avec {client_address}")
 
-while True:
-    # Attente de données depuis le client
-    data = secure_client_socket.recv(1024).decode('utf-8')
+    # Wrapping du socket dans SSL
+    secure_client_socket = ssl.wrap_socket(
+        client_socket, keyfile=KEYFILE, certfile=CERTFILE, server_side=True)
 
-    if not data:
-        break
+    # Envoi du certificat au client
+    with open(CERTFILE, 'rb') as cert_file:
+        server_cert_bytes = cert_file.read()
 
-    print(f"Client: {data}")
+    secure_client_socket.send(server_cert_bytes)
 
-    # Réponse au client
-    message = input("Serveur: ")
-    secure_client_socket.send(message.encode('utf-8'))
+    while True:
+        # Attente de données depuis le client
+        data = secure_client_socket.recv(1024).decode('utf-8')
 
-# Fermeture des connexions
-secure_client_socket.close()
-server_socket.close()
+        if not data:
+            break
+
+        print(f"Client: {data}")
+
+        # Réponse au client
+        message = input("Serveur: ")
+        secure_client_socket.send(message.encode('utf-8'))
+
+except ssl.SSLError as e:
+    print(f"Une erreur SSL s'est produite : {e}")
+
+except Exception as e:
+    print(f"Une erreur inattendue s'est produite : {e}")
+
+finally:
+    # Fermeture des connexions
+    if secure_client_socket:
+        secure_client_socket.close()
+
+    server_socket.close()
